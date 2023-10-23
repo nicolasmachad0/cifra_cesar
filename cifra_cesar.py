5893116e1c624216802ecfc115fed2235a7087cd
Nicolas Machado Fogaça <124478422+nicolasmachad0@users.noreply.github.com>
2023-10-23 10:44:42 -0300

    Update cifra_cesar.py

--- cifra_cesar.py
+++ cifra_cesar.py
@@ -6,74 +6,82 @@
 # NICHOLAS PEREIRA DE BRITES NASCIMENTO
 # NICOLAS MACHADO FOGAÇA
 
+# Função que implementa a cifra de César
+
+def cesar(texto, chave, agir): 
+    if len(texto) > 128:
+        return "O texto deve ter no máximo 128 caracteres." # Define um limeite de 128 caracteres 
+    resultado = '' # Inicializa uma string vazia para armazenar o resultado
+    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' #  alfabeto que vai ser utilizado
+
+# Itera cada caractere no texto de entrada
+    for char in texto:     
+        if char.isalpha(): # Verifica se o caractere é uma letra
+            char_maiusculo = char.upper() # Converte o caractere para maiúsculas
+            
+            # Verifica a ação a ser executada (criptografar ou descriptografar)
+            if agir == 'criptografar':
+                if char_maiusculo in alfabeto: # Verifica se o caractere está no alfabeto
+                    calculo = (alfabeto.index(char_maiusculo) + chave) % 26  # Calcula a nova posição da letra após a criptografia
+                    texto_cifrado = alfabeto[calculo]# Obtém o caractere cifrado
+
+                    if char.islower(): 
+                        texto_cifrado = texto_cifrado.lower() # Mantém o caractere em minúsculas
+                else:
+                    texto_cifrado = char # Mantém caracteres que não estão no alfabeto inalterados
+
+            elif agir == 'descriptografar':
+
+                if char_maiusculo in alfabeto:  # Calcula a nova posição da letra após a descriptografia
+                    calculo = (alfabeto.index(char_maiusculo) - chave) % 26 # Calcula a nova posição da letra após a descriptografia
+                    texto_cifrado = alfabeto[calculo] # Obtém o caractere descriptografado
+
+                    if char.islower():
+                        texto_cifrado = texto_cifrado.lower() # Mantém o caractere em minúsculas
+                else:
+                    texto_cifrado = char # Mantém caracteres que não estão no alfabeto inalterados
+
+            resultado += texto_cifrado # Adiciona o caractere cifrado ou descriptografado ao resultado
+        else:
+            resultado += char # Mantém caracteres não alfabéticos inalterados
+
+    return resultado # Retorna o resultado final
 
 # Função que implementa a cifra de César
-def cifra_cesar(texto, chave, acao):
-    resultado = ''  # Inicializa uma string vazia para armazenar o resultado
-    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # Define o alfabeto a ser usado na cifra de César
 
-    # Itera sobre cada caractere no texto de entrada
-    for char in texto:
-        if char.isalpha():  # Verifica se o caractere é uma letra
-            char_maiusculo = char.upper()  # Converte o caractere para maiúsculas
+print('APS 2023 - Cifra de Cesar')
 
-            # Verifica a ação a ser executada (criptografar ou descriptografar)
-            if acao == 'criptografar':
-                if char_maiusculo in alfabeto:  # Verifica se o caractere está no alfabeto
-                    # Calcula a nova posição da letra após a criptografia
-                    indice = (alfabeto.index(char_maiusculo) + chave) % 26
-                    char_cifrado = alfabeto[indice]  # Obtém o caractere cifrado
-
-                    if char.islower():
-                        char_cifrado = char_cifrado.lower()  # Mantém o caractere em minúsculas, se for o caso
-                else:
-                    char_cifrado = char  # Mantém caracteres que não estão no alfabeto inalterados
-            elif acao == 'descriptografar':
-                if char_maiusculo in alfabeto:
-                    # Calcula a nova posição da letra após a descriptografia
-                    indice = (alfabeto.index(char_maiusculo) - chave) % 26
-                    char_cifrado = alfabeto[indice]  # Obtém o caractere descriptografado
-
-                    if char.islower():
-                        char_cifrado = char_cifrado.lower()  # Mantém o caractere em minúsculas, se for o caso
-                else:
-                    char_cifrado = char  # Mantém caracteres que não estão no alfabeto inalterados
-
-            resultado += char_cifrado  # Adiciona o caractere cifrado ou descriptografado ao resultado
-        else:
-            resultado += char  # Mantém caracteres não alfabéticos inalterados
-
-    return resultado  # Retorna o resultado final
-
-# Loop principal para exibir o menu e processar a escolha do usuário
 while True:
-    
-    print("Menu:")
-    print("1. Criptografar")
-    print("2. Descriptografar")
-    print("3. Sair")
-    
-    menu = input("Escolha uma opção: ")  # Solicita a escolha do usuário
-    
-    if menu == '1':  # Se a escolha for 1, criptografa o texto
+    print('\n')
+    print('1 - Critptografar')
+    print('2 - Descriptografar')
+    print ('3 - Sair')
 
-        texto = input("Digite o texto a ser criptografado: ")  # Solicita o texto de entrada
-        chave = int(input("Digite a chave da cifra de César: "))  # Solicita a chave de criptografia
-        texto_criptografado = cifra_cesar(texto, chave, 'criptografar')  # Chama a função para criptografar
-        print("Texto criptografado: " + texto_criptografado)  # Exibe o resultado criptografado
+    painel = input('O que deseja: ')
 
-    elif menu == '2':  # Se a escolha for 2, descriptografa o texto
+    if painel == '1': 
 
-        texto = input("Digite o texto a ser descriptografado: ")  # Solicita o texto de entrada
-        chave = int(input("Digite a chave da cifra de César: "))  # Solicita a chave de descriptografia
-        texto_descriptografado = cifra_cesar(texto, chave, 'descriptografar')  # Chama a função para descriptografar
-        print("Texto descriptografado: " + texto_descriptografado)  # Exibe o resultado descriptografado
+        texto = input("Digite o texto: ")  
+        if len(texto) > 128:
+            continue
+        chave = int(input("Digite a chave da cifra de César: "))  
+        criptografado = cesar(texto, chave, 'criptografar')  
+        print("Texto criptografado: " + criptografado)  
 
-    elif menu == '3':  # Se a escolha for 3, sai do programa
+    elif painel == '2':  
+
+        texto = input("Digite o texto: ")
+        if len(texto) > 128:
+            chave = int(input("Digite a chave da cifra de César: ")) 
+            continue 
+        descriptografado = cesar(texto, chave, 'descriptografar')  
+        print("Texto descriptografado: " + descriptografado)  
+
+    elif painel == '3': 
 
         print("Saindo...")
         break
 
-    else:  # Se a escolha for inválida
+    else:  
 
-        print("Opção inválida. Tente novamente.")  # Exibe uma mensagem de erro
+        print("Opção inválida. Tente novamente.")
